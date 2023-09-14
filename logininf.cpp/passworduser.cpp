58827d4256fd90bd70b9ea8c4c8d82178dca6117
kewadahal <110725776+ProfKdX7@users.noreply.github.com>
2023-09-14 13:41:02 +0545

    used object and classes

--- logininf.cpp/passworduser.cpp
+++ logininf.cpp/passworduser.cpp
@@ -1,54 +1,58 @@
-// a simple cpp program which get the username and password from user and store them in a file
-// and implement the file element in login phase which help user to make an good password login interface with .txt database
+/*A simple password and username login interface you can inject to your code and make your work easier thank me later */
+
 #include <iostream>
 #include <fstream>
 #include <string>
 
 using namespace std;
 
-// Function to register a new user
-void RegisterUser() {
-    string username, password;
-    cout << "Enter a username: ";
-    cin >> username;
-    cout << "Enter a password: ";
-    cin >> password;
-    ofstream file("password.txt", ios::app);
-    if (!file.is_open()) {
-        cerr << "Error: Could not open the file." << endl;
-        return;
-    }
+class PasswordManager {
+public:
+    // Function to register a new user
+    void RegisterUser() {
+        string username, password;
+        cout << "Enter a username: ";
+        cin >> username;
+        cout << "Enter a password: ";
+        cin >> password;
 
-    // Write the username and password to the file
-    file << username << ' ' << password << endl;
-
-    cout << "Registration successful! You can now log in." << endl;
-    file.close();
-}
-
-// Function to check if a username and password match for the same person
-bool AuthenticateUser(const string& username, const string& password) {
-    string storedUsername, storedPassword;
-    ifstream file("password.txt");
-    if (!file.is_open()) {
-        cerr << "Error: Could not open the file." << endl;
-        return false;
-    }
-
-    while (file >> storedUsername >> storedPassword) {
-        if (username == storedUsername && password == storedPassword) {
-            file.close();
-            return true; // User found
+        ofstream file("password.txt", ios::app);
+        if (!file.is_open()) {
+            cerr << "Error: Could not open the file." << endl;
+            return;
         }
+
+        // Write the username and password to the file
+        file << username << ' ' << password << endl;
+
+        cout << "Registration successful! You can now log in." << endl;
+        file.close();
     }
 
-    file.close();
-    return false; // User not found
-}
+    // Function to check if a username and password match for the same person
+    bool AuthenticateUser(const string& username, const string& password) {
+        string storedUsername, storedPassword;
+        ifstream file("password.txt");
+        if (!file.is_open()) {
+            cerr << "Error: Could not open the file." << endl;
+            return false;
+        }
+
+        while (file >> storedUsername >> storedPassword) {
+            if (username == storedUsername && password == storedPassword) {
+                file.close();
+                return true; // User found
+            }
+        }
+
+        file.close();
+        return false; // User not found
+    }
+};
 
 int main() {
+    PasswordManager passwordManager;
     int choice;
-    string username, password;
 
     cout << "Welcome to the Login System!" << endl;
     do {
@@ -57,20 +61,23 @@ int main() {
 
         switch (choice) {
             case 1:
-                RegisterUser();
+                passwordManager.RegisterUser();
                 break;
-            case 2:
+            case 2: {
+                string username, password;
                 cout << "Enter your username: ";
                 cin >> username;
                 cout << "Enter your password: ";
                 cin >> password;
-                if (AuthenticateUser(username, password)) {
+                if (passwordManager.AuthenticateUser(username, password)) {
                     cout << "Authentication successful! Access granted." << endl;
+                    //call the method of the main program to be executed here 
 
                 } else {
                     cout << "Authentication failed. Please try again." << endl;
                 }
                 break;
+            }
             case 3:
                 cout << "Goodbye!" << endl;
                 break;
