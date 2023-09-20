25d72cbb07e75b59acbb5e89bb9467498204e670
kewadahal <110725776+ProfKdX7@users.noreply.github.com>
2023-09-20 10:52:20 +0545

    added dupliate username prevention

--- logininf.cpp/passworduser.cpp
+++ logininf.cpp/passworduser.cpp
@@ -6,40 +6,66 @@
 
 using namespace std;
 
-class PasswordManager {
+class PasswordManager
+{
 public:
+//function to clear the display
+    void cleardisplay(){
+        system("cls");
+    }
     // Function to register a new user
-    void RegisterUser() {
+    void RegisterUser()
+    {
         string username, password;
         cout << "Enter a username: ";
         cin >> username;
         cout << "Enter a password: ";
         cin >> password;
 
-        ofstream file("password.txt", ios::app);
-        if (!file.is_open()) {
+        ifstream file("password.txt");
+        string storedUsername;
+
+        while (file >> storedUsername)
+        {
+            if (username == storedUsername)
+            {
+                file.close();
+                cerr << "Error: Username already taken. Please choose a different username." << endl;
+                return;
+            }
+        }
+
+        file.close();
+
+        ofstream outFile("password.txt", ios::app);
+        if (!outFile.is_open())
+        {
             cerr << "Error: Could not open the file." << endl;
             return;
         }
 
         // Write the username and password to the file
-        file << username << ' ' << password << endl;
+        outFile << username << ' ' << password << endl;
 
         cout << "Registration successful! You can now log in." << endl;
-        file.close();
+        outFile.close();
+        cleardisplay();
     }
-
     // Function to check if a username and password match for the same person
-    bool AuthenticateUser(const string& username, const string& password) {
+    bool AuthenticateUser(const string &username, const string &password)
+    {
         string storedUsername, storedPassword;
         ifstream file("password.txt");
-        if (!file.is_open()) {
+        if (!file.is_open())
+        {
             cerr << "Error: Could not open the file." << endl;
             return false;
         }
 
-        while (file >> storedUsername >> storedPassword) {
-            if (username == storedUsername && password == storedPassword) {
+        while (file >> storedUsername >> storedPassword)
+        {
+            if (username == storedUsername && password == storedPassword)
+            {
                 file.close();
                 return true; // User found
             }
@@ -50,64 +76,48 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     PasswordManager passwordManager;
     int choice;
 
     cout << "Welcome to the Login System!" << endl;
-    do {
+    do
+    {
         cout << "1. Register\n2. Login\n3. Exit\nEnter your choice: ";
         cin >> choice;
 
-        switch (choice) {
-            case 1:
-                passwordManager.RegisterUser();
-                break;
-            case 2: {
-                string username, password;
-                cout << "Enter your username: ";
-                cin >> username;
-                cout << "Enter your password: ";
-                cin >> password;
-                if (passwordManager.AuthenticateUser(username, password)) {
-                    cout << "Authentication successful! Access granted." << endl;
-                    //call the method of the main program to be executed here 
-                    /*
-                    
-                    
-                    
-                    
-                    
-                    YOUR MAIN METHOD/CLASS/OBJ HERE
-                    ------         ------      ------------
-                    ------         ------      ------------
-                    ------         ------           ---
-                    ------         ------           ---
-                    ---------------------           ---
-                    ---------------------           ---
-                    ---------------------           ---
-                    ------         ------           ---
-                    ------         ------           ---
-                    ------         ------      ------------
-                    ------         ------      ------------
-                    
-                    
-                    
-                    
-                    
-                    */
-
-                } else {
-                    cout << "Authentication failed. Please try again." << endl;
-                }
-                break;
+        switch (choice)
+        {
+        case 1:
+            passwordManager.RegisterUser();
+            break;
+        case 2:
+        {
+            string username, password;
+            cout << "Enter your username: ";
+            cin >> username;
+            cout << "Enter your password: ";
+            cin >> password;
+            if (passwordManager.AuthenticateUser(username, password))
+            {
+                cout << "Authentication successful! Access granted." << endl;
+                // call the method of the main program to be executed here
+                passwordManager.cleardisplay();
+            
             }
-            case 3:
-                cout << "Goodbye!" << endl;
-                break;
-            default:
-                cout << "Invalid choice. Please try again." << endl;
-                break;
+            else
+            {
+                cout << "Authentication failed. Please try again." << endl;
+            }
+            break;
+        }
+        case 3:
+            cout << "Goodbye!" << endl;
+            break;
+        default:
+            cout << "Invalid choice. Please try again." << endl;
+            break;
         }
     } while (choice != 3);
 
