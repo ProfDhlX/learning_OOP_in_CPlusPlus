46b4955a7d34e935551f082e181e94daef37dc38
kewadahal <110725776+ProfKdX7@users.noreply.github.com>
2023-10-06 07:49:28 +0545

    oddeven withiou modulas

--- newf/main.cpp
+++ newf/main.cpp
@@ -1,1 +1,22 @@
-//code coming
+//odd even withou modulas operator
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    int temp = a;
+    int ttemp = a/2;
+
+   
+
+    if(ttemp*2 == temp){
+        cout<<"Number is even";
+    } else{
+        cout<<"Number is odd";
+    }
+
+    return 0;
+    
+}
