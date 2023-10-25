811e009e1903a63add32f84f538d905aa3f8242a
kewadahal <110725776+ProfKdX7@users.noreply.github.com>
2023-10-25 16:33:58 +0545

    initial delare

--- newproj/main.cpp
+++ newproj/main.cpp
@@ -1,1 +1,14 @@
 //---- COMING SOON ----//
+#include<iostream>
+using namespace std;
+class program{
+    public:
+    void initial(){
+
+    }
+
+};
+int main(){
+    program obj;
+    obj.initial;
+}
